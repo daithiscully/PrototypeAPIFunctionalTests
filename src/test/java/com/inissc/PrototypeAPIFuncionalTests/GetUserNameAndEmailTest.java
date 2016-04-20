26025d9bc1a71e93aecbfc33bccaac93922016b5
David Scully <davidscully129@gmail.com>
2016-04-20 15:59:05 +0100

    Added in a JSON parser test

--- src/test/java/com/inissc/PrototypeAPIFuncionalTests/GetUserNameAndEmailTest.java
+++ src/test/java/com/inissc/PrototypeAPIFuncionalTests/GetUserNameAndEmailTest.java
@@ -5,15 +5,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Iterator;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GetUserNameAndEmailTest {
 
-	@Test
-	public void excuteGet() throws IOException {
+	/*@Test
+	public void excuteGetName() throws IOException {
 		StringBuilder result = new StringBuilder();
-		URL url = new URL("http://localhost:3000/api/user/1");
+		URL url = new URL("http://localhost:3000/api/username/1");
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
 		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
@@ -23,6 +28,44 @@ public class GetUserNameAndEmailTest {
 		}
 		rd.close();
 		System.out.println("Result: " + result);
-		// Assert.assertTrue(result.);
+
+		
+		Assert.assertTrue(result.equals("{\"id\":\"1\",\"name\":\"Pink Floyd\"}"));
+	}*/
+	
+	@Test
+	public void excuteGetName() throws IOException {
+		JSONParser parser = new JSONParser();
+		 
+        try {
+        	StringBuilder result = new StringBuilder();
+    		URL url = new URL("http://localhost:3000/api/username/1");
+    		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    		conn.setRequestMethod("GET");
+    		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+    		String line;
+    		while ((line = rd.readLine()) != null) {
+    			result.append(line);
+    		}
+    		rd.close();
+            Object obj = parser.parse(result.toString());
+ 
+            JSONObject jsonObject = (JSONObject) obj;
+
+            String id = (String) jsonObject.get("id");
+            String name = (String) jsonObject.get("name");
+            JSONArray artistInfo = (JSONArray) jsonObject.get("Company List");
+
+            System.out.println("ID: " + id);
+            System.out.println("Name: " + name);
+            System.out.println("\nCompany List:");
+            Iterator<String> iterator = artistInfo.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
 }
