1d799c592d88cdc053b179f6b1d39c219f1bffa5
David Scully <davidscully129@gmail.com>
2016-04-20 16:05:17 +0100

    Added in the email api test

--- src/test/java/com/inissc/PrototypeAPIFuncionalTests/GetUserNameAndEmailTest.java
+++ src/test/java/com/inissc/PrototypeAPIFuncionalTests/GetUserNameAndEmailTest.java
@@ -15,23 +15,7 @@ import org.testng.annotations.Test;
 
 public class GetUserNameAndEmailTest {
 
-	/*@Test
-	public void excuteGetName() throws IOException {
-		StringBuilder result = new StringBuilder();
-		URL url = new URL("http://localhost:3000/api/username/1");
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		String line;
-		while ((line = rd.readLine()) != null) {
-			result.append(line);
-		}
-		rd.close();
-		System.out.println("Result: " + result);
-
-		
-		Assert.assertTrue(result.equals("{\"id\":\"1\",\"name\":\"Pink Floyd\"}"));
-	}*/
+	String username;
 	
 	@Test
 	public void excuteGetName() throws IOException {
@@ -58,6 +42,37 @@ public class GetUserNameAndEmailTest {
             System.out.println("ID: " + id);
             System.out.println("Name: " + name);
             Assert.assertTrue(name.equals("Pink Floyd"));
+            username = name;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+	
+	@Test(dependsOnMethods="excuteGetName")
+	public void excuteGetEmailFromName() throws IOException {
+		JSONParser parser = new JSONParser();
+		 
+        try {
+        	StringBuilder result = new StringBuilder();
+    		URL url = new URL("http://localhost:3000/api/useremail/" + username);
+    		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    		conn.setRequestMethod("GET");
+    		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+    		String line;
+    		while ((line = rd.readLine()) != null) {
+    			result.append(line);
+    		}
+    		rd.close();
+            Object obj = parser.parse(result.toString());
+ 
+            JSONObject jsonObject = (JSONObject) obj;
+
+            String id = (String) jsonObject.get("id");
+            String email = (String) jsonObject.get("email");
+
+            System.out.println("ID: " + id);
+            System.out.println("email: " + email);
+            Assert.assertTrue(email.equals("PinkFloyd@gmail.com"));
  
         } catch (Exception e) {
             e.printStackTrace();
