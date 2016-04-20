bc12669f123a22d8d0cd25ec5bb2f8224dd9db6c
David Scully <davidscully129@gmail.com>
2016-04-20 16:07:22 +0100

    Fixes

--- src/test/java/com/inissc/PrototypeAPIFuncionalTests/GetUserNameAndEmailTest.java
+++ src/test/java/com/inissc/PrototypeAPIFuncionalTests/GetUserNameAndEmailTest.java
@@ -48,7 +48,7 @@ public class GetUserNameAndEmailTest {
         }
 	}
 	
-	@Test(dependsOnMethods="excuteGetName")
+	@Test(dependsOnMethods={ "excuteGetName" })
 	public void excuteGetEmailFromName() throws IOException {
 		JSONParser parser = new JSONParser();
 		 
