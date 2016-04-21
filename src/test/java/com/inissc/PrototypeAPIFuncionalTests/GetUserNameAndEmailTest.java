88368a93bee89dd30c711ba97e507ee53d1ccfec
daithiscully <davidscully129@gmail.com>
2016-04-21 14:14:40 +0100

    Fixed the second test. It was having issue with the white space in the username so I stripped it out

--- src/test/java/com/inissc/PrototypeAPIFuncionalTests/GetUserNameAndEmailTest.java
+++ src/test/java/com/inissc/PrototypeAPIFuncionalTests/GetUserNameAndEmailTest.java
@@ -85,37 +85,4 @@ public class GetUserNameAndEmailTest {
         }
         System.out.println("Finished test: excuteGetEmailFromName()");
 	}
-
-
-    public String makeAPIRequest(){
-        System.out.println("Started test: excuteGetEmailFromName()\nName checking: " + username);
-        JSONParser parser = new JSONParser();
-         
-        try {
-            StringBuilder result = new StringBuilder();
-            URL url = new URL(PROTOTYPE_URL + "/useremail/" + username);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line;
-            while ((line = rd.readLine()) != null) {
-                result.append(line);
-            }
-            rd.close();
-            Object obj = parser.parse(result.toString());
- 
-            JSONObject jsonObject = (JSONObject) obj;
-
-            String email = (String) jsonObject.get("email");
-
-            System.out.println("email: " + email);
-            //Assert.assertTrue(email.equals("PinkFloyd@gmail.com"));
-            Assert.assertTrue(email.equals("NoEmailGiven@gmail.com"));
- 
-            conn.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        System.out.println("Finished test: excuteGetEmailFromName()");
-    }
 }
